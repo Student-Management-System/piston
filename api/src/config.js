3be7c9fcfd5218d25061977262023c81857aa2ea
klingebiel <lukas.klingebiel@web.de>
2022-12-02 11:15:46 +0100

    increase run timeout to 10s
    We have combined the compilation and run steps into a single script. Therefore, longer timeout may be necessary.

--- api/src/config.js
+++ api/src/config.js
@@ -86,7 +86,7 @@ const options = {
     },
     run_timeout: {
         desc: 'Max time allowed for run stage in milliseconds',
-        default: 3000, // 3 seconds
+        default: 10000, // 10 seconds
         parser: parse_int,
         validators: [(x, raw) => !is_nan(x) || `${raw} is not a number`],
     },
